b113b10bfe7947d318de75059b8e7afa4ac8f704
fakestarbaby <fakestarbaby@gmail.com>
2011-12-06 09:56:44 +0900

    初回コミット。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleHerokuRails::Application.config.secret_token = 'a1019e995ca6ede9ecf14fa7a8dfa5cdb9f41c2670b2a0baa2784e82f49c0e97a5e0789972a32f75a6415c9fce4a4398a58b9d49a841380cd9cff2d6b5410c16'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+SampleHerokuRails::Application.routes.draw do
+  root :to => "welcome#index"
+end
